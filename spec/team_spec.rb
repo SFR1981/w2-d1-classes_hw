e71237190094e6abb64a7b48422353bc13c6656b
Stephen Rooney <s.f.rooney@gmail.com>
2018-05-28 16:42:18 +0100

    added add player

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -5,25 +5,45 @@ require_relative("../team")
 
 class StudentTest < MiniTest::Test
 
+  # def test_team_name
+  #   team = Team.new("StarBug", ["Lister", "Rimmer", "Kryten"], "Doug Naylor")
+  #   assert_equal("StarBug", team.get_name())
+  # end
+
   def test_team_name
     team = Team.new("StarBug", ["Lister", "Rimmer", "Kryten"], "Doug Naylor")
-    assert_equal("StarBug", team.get_name())
+
+
+    assert_equal("StarBug" , team.team_name())
+
   end
 
   def test_players
     team = Team.new("StarBug", ["Lister", "Rimmer", "Kryten"], "Doug Naylor")
-    assert_equal(["Lister", "Rimmer", "Kryten"], team.get_players())
+    assert_equal(["Lister", "Rimmer", "Kryten"], team.players())
   end
 
   def test_coach
     team = Team.new("StarBug", ["Lister", "Rimmer", "Kryten"], "Doug Naylor")
-    assert_equal("Doug Naylor", team.get_coach())
+    assert_equal("Doug Naylor", team.coach())
   end
 
   def test_set_new_coach
     team = team = Team.new("StarBug", ["Lister", "Rimmer", "Kryten"], "Doug Naylor")
-    team.set_new_coach("Ted Danson")
-    assert_equal("Ted Danson", team.get_coach())
+    team.coach = "Ted Danson"
+    assert_equal("Ted Danson", team.coach())
+  end
+
+  def test_add_player
+    team = Team.new("StarBug", ["Lister", "Rimmer", "Kryten"], "Doug Naylor")
+    team.add_player("Cat")
+    assert_equal(["Lister", "Rimmer", "Kryten", "Cat"], team.players())
+
+  end
+
+
+  def test_check_if_player
+
   end
 
 
--- team.rb
+++ team.rb
@@ -1,5 +1,9 @@
 class Team
 
+
+attr_reader(:team_name, :players, :coach)
+attr_writer(:players, :coach)
+
   def initialize(team_name, players, coach)
 
     @team_name = team_name
@@ -8,26 +12,29 @@ class Team
 
   end
 
-  def get_name()
-    return @team_name
+  # def get_name()
+  #   return @team_name
+  # end
+  #
+  # def get_players()
+  #   return @players
+  # end
+  #
+  # def get_coach()
+  #
+  #   return @coach
+  # end
+  #
+  # def set_new_coach(new_coach)
+  #   @coach = new_coach
+  #
+  #
+  # end
+
+  def add_player(new_player_name)
+    @players = @players << new_player_name
+
   end
-
-  def get_players()
-    return @players
-  end
-
-  def get_coach()
-
-    return @coach
-  end
-
-  def set_new_coach(new_coach)
-    @coach = new_coach 
-
-
-  end
-
-
 
 
 
